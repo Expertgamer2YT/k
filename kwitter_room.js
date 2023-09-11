6de349b0798f0154bb49cfb44d9f2492d29ae71b
Expertgamer2YT <91056870+Expertgamer2YT@users.noreply.github.com>
2023-09-11 17:37:39 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -29,7 +29,7 @@ var firebaseConfig = {
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
        Room_names = childKey;
        console.log("room_name"+Room_names);
-       row = "<div class = 'room_name' id =" + Room_names+"onclick = 'redirectToRoomName(this.id)' > #"+Room_names+"</div> <hr>";
+       row = "<div class = 'room_name' id ="+Room_names+" onclick='redirectToRoomName(this.id)'> #"+Room_names+"</div><hr>";
        document.getElementById("output").innerHTML+= row;
       
       });});}
@@ -37,7 +37,7 @@ getData();
  function redirectToRoomName(name){
       console.log(name);
       localStorage.setItem("room_name",name);
-      window.location="kwitter_page.html"
+      window.location= "kwitter_page.html";
 
  }
 
